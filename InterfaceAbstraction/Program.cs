39a60663f3304ed948941cadcf9d43b7a1bbb92c
sukra <sukra@DESKTOP-OB7AJV3>
2025-02-02 13:13:04 +0300

    Add project files.

---
+++ InterfaceAbstraction/Program.cs
@@ -0,0 +1,33 @@
+﻿//Çalışan örneklerini oluşturuyoruz.
+using InterfaceAbstraction;
+
+Iworker softwareDeveloper = new SoftwareDeveloper
+{
+    Name = "Şükra",
+    Surname = "Kayhan",
+    Department = "Yazılım Geliştirme"
+};
+
+Iworker projectManager = new ProjectManager
+{
+    Name = "ayse",
+    Surname = "yılmaz",
+    Department = "Proje Yönetimi"
+};
+
+Iworker salesRepresentative = new SalesRepresentative
+{
+    Name = "ceren",
+    Surname = "korkmaz",
+    Department = "Satış Danışmanı"
+};
+
+//Görevleri çağırma
+Console.WriteLine($"{softwareDeveloper.Name} {softwareDeveloper.Surname}: ");
+softwareDeveloper.Duty();
+
+Console.WriteLine($"{projectManager.Name} {projectManager.Surname}: ");
+projectManager.Duty();
+
+Console.WriteLine($"{salesRepresentative.Name} {salesRepresentative.Surname}: ");
+salesRepresentative.Duty();
